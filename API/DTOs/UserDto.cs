1e848b14bcae8abfaa57a25b8888aa06663cc955
Ceddyp19 <ceddyp117@gmail.com>
2022-06-01 09:58:02 -0500

    End of Section 4

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+//this is the object we're returning when the user logs in/ registers
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public string Username { get; set; }
+
+        public string Token { get; set; }
+    }
+}
